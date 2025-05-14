58970e3e6e2e20b15ba56aa97e66a92fe6b44809
sophia <laloux.sophia@gmail.com>
2025-05-14 16:27:51 +0200

    add homepage

---
+++ src/components/TheHome.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HomeCard from '../components/home/HomeCard.vue';
+</script>
+
+<template>
+    <div class="home-container">
+        <div class="home-grid">
+            <HomeCard :route="'/side-menu'" :desc="'Side Menu'"/>
+            <HomeCard :route="'/timeline'" :desc="'Timeline'"/>
+            <HomeCard :route="'/grid-cards'" :desc="'Grid cards'"/>
+        </div>
+    </div>
+</template>
---
+++ src/components/home/HomeCard.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  route: string
+  desc: string
+}>()
+</script>
+
+<template>
+    <div class="home-card">
+        <RouterLink :to="route">{{ desc }}</RouterLink>
+    </div>
+</template>
